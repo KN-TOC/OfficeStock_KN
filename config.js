fbf5be982aacc304bb3d004b06ed3c1feb0aad15
KN-TOC <worachatphattarawongamorn@gmail.com>
2026-03-31 15:25:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const APP_CONFIG = {
+  supabaseUrl: 'https://iwawmenevoeuwwivbsfk.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3YXdtZW5ldm9ldXd3aXZic2ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMzU5NTgsImV4cCI6MjA4OTkxMTk1OH0.egdEf46rkDNDLzJlWDNPgJV0oN_fE5D7woKM80RW8Ow'
+};
